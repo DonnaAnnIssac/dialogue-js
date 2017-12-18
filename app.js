cfd79607e49b402f5a50fbd661321743f73d2b4c
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-18 17:01:18 +0530

    Set up database connection

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const express = require('express')
 const app = express()
+const routes = require('./routes')
+const mongodb = require('mongodb')
+let db
+let MongoClient = require('mongodb').MongoClient
 
-app.listen(5000, () => console.log('Listening on port 5000'))
+MongoClient.connect('mongodb://localhost:27017/redditClone', (err, database) => {
+  if (err) console.error(err)
+  db = database
+  console.log('Connected to databse')
+})
+
+app.use('/api', routes)
+
+module.exports = {app, MongoClient}
