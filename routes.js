d58b899e32b1745f1789da90ecbbcb22155b6e97
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-20 10:21:14 +0530

    Add routes for posts and comments

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const basic = require('./controllers/basic')
 const user = require('./controllers/userController')
+const post = require('./controllers/postController')
+const comment = require('./controllers/cmntController')
 
 const routes = express.Router()
 
@@ -9,5 +11,11 @@ routes.get('/', basic.get)
 
 // User routes
 routes.post('/signup', user.post)
+
+// Post routes
+routes.post('/post', post.create)
+
+// Comment routes
+routes.post('/comment', comment.post)
 
 module.exports = routes
