a4ed87110b0084c567b8c42a594a6b97a16692ed
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-20 10:25:59 +0530

    Add model for comments

---
+++ models/commentModel.js
@@ -0,0 +1,11 @@
+class Comment {
+    constructor(obj) {
+        this.link = obj.link,
+        this.text = obj.text,
+        this.author = obj.author,
+        this.time = obj.time,
+        this.score = 0
+    }
+ }
+
+ module.exports = Comment
