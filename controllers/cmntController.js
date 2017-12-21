a8364da706dafbc8da8157405e72921bb3717016
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-21 11:57:14 +0530

    Add methods to update and delete posts and update score of posts

--- controllers/cmntController.js
+++ controllers/cmntController.js
@@ -1,3 +1,4 @@
+
 const CommentModel = require('./../models').comment
 const commentController = {}
 
@@ -6,6 +7,35 @@ commentController.post = (req, res) => {
   req.app.db.collection('comments').insertOne(comment, (err, result) => {
     if (err) throw err
     console.log('Document inserted')
+    res.json(result)
+  })
+}
+
+commentController.delete = (req, res) => {
+  req.app.db.collection('comments')
+  .deleteOne({
+    "_id" : ObjectId(req.body.id),
+    "author_id" : ObjectId(req.body.my_id)
+  })
+}
+
+commentController.update = (req, res) => {
+  let keys = Object.keys(req.body.values)
+  let values = {}
+  keys.forEach((key) => {
+    values[key] = req.body.values[key]
+  })
+  req.app.db.collection('comments')
+  .updateOne({ "_id" : ObjectId(req.body.id) }, values, (err, result) => {
+    if (err) throw err
+    console.log('Updated one document')
+  })
+}
+
+commentController.updateScore = (req, res) => {
+  req.app.db.collection('comments')
+  .updateOne({"_id" : ObjectId(req.body.id)}, {"score" : req.body.score}, (err, result) => {
+    if (err) throw err
     res.json(result)
   })
 }
