99a4915d20b777b96f0cb2909a48eec7552a0219
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-21 11:55:39 +0530

    Add reference to user in model instances

--- models/postModel.js
+++ models/postModel.js
@@ -1,9 +1,12 @@
+const ObjectId = require('mongodb').ObjectID
+
 class Post {
    constructor(obj) {
        this.title = obj.title,
        this.link = obj.link,
        this.text = obj.text,
        this.author = obj.author,
+       this.author_id = ObjectId(obj.author_id,)
        this.score = 0
    }
 }
