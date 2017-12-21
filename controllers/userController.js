1ac25eb5bee252720602a739a8486f10ebb1281b
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-22 05:07:37 +0530

    Add method for user login requests and to validate user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,11 +3,34 @@ const userController = {}
 
 userController.post = (req, res) => {
   // add validation
+  if(!validateName(req.body)) res.json("This user name exists. Enter another name")
+
   const user = new UserModel(req.body)
   req.app.db.collection('users').insertOne(user, (err, result) => {
     if (err) throw err
     console.log('Document inserted')
     res.json(result)
+  })
+}
+
+const validateName = (body) => {
+  req.app.db.collection('users')
+  .find({"userName" : body.userName})
+  .toArray((err, result) => {
+    if (err) throw err
+    if(result.length === 0) return true
+    else return false
+  })
+}
+
+userController.login = (req, res) => {
+  req.app.db.collection('users')
+  .find({"userName" : req.body.userName})
+  .toArray((err, result) => {
+    if (err) throw err
+    if (result.length === 0) res.json("Invalid User Name")
+    if (result[0].password !== req.body.password) res.json("Password incorrect")
+    else res.json(result)
   })
 }
 
