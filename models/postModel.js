1e500153ed36e7155e2181119061f243c4a8f569
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-22 06:19:04 +0530

    rectify syntax error

--- models/postModel.js
+++ models/postModel.js
@@ -6,7 +6,7 @@ class Post {
        this.link = obj.link,
        this.text = obj.text,
        this.author = obj.author,
-       this.author_id = ObjectId(obj.author_id,)
+       this.author_id = ObjectId(obj.author_id),
        this.score = 0
    }
 }
