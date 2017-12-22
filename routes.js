626d28a0cd15dc0d95c69b5529400c8942abc73f
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-22 09:55:07 +0530

    Remove basicController

--- routes.js
+++ routes.js
@@ -1,13 +1,9 @@
 const express = require('express')
-const basic = require('./controllers/basic')
 const user = require('./controllers/userController')
 const post = require('./controllers/postController')
 const comment = require('./controllers/cmntController')
 
 const routes = express.Router()
-
-// Basic routes
-routes.get('/', basic.get)
 
 // User routes
 routes.post('/signup', user.post)
