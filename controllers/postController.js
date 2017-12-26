6b9e2f520937f7489e1c9f18d52bcbd196fe84bd
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-26 12:33:35 +0530

    Include error handlers and define uniform response format

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,66 +5,64 @@ const postController = {}
 postController.create = (req, res) => {
   const post = new PostModel(req.body)
   req.app.db.collection('posts').insertOne(post, (err, result) => {
-    if (err) throw err
-    console.log('Document inserted')
-    res.json(result)
+    if (err) res.json({'status': 'fail', 'data': err})
+    else res.json({'status': 'success', 'data': result})
   })
 }
 
 postController.getAll = (req, res) => {
   req.app.db.collection('posts').find().toArray((err, result) => {
-    if (err) throw err
-    console.log('Retrieved all posts from collection')
-    res.json(result)
+    if (err) res.json({'status': 'fail', 'data': err})
+    else res.json({'status': 'success', 'data': result})
   })
 }
 
 postController.show = (req, res) => {
   req.app.db.collection('posts')
-  .find({"_id" : ObjectId(req.query.id)})
+  .find({'_id': ObjectId(req.query.id)})
   .toArray((err, result) => {
-    if (err) throw err
-    req.app.db.collection('comments')
-    .find({"post_id" : ObjectId(req.query.id)})
-    .toArray((err, results) => {
-      res.json({"post" : result[0], "comments" : results})
-    })
+    if (err) res.json({'status': 'fail', 'data': err})
+    else {
+      req.app.db.collection('comments')
+      .find({'post_id': ObjectId(req.query.id)})
+      .toArray((err, results) => {
+        if (err) res.json({'status': 'fail', 'data': {'post': result[0], 'comments': err}})
+        else res.json({'status': 'success', 'data': {'post': result[0], 'comments': results}})
+      })
+    }
   })
 }
 
 postController.delete = (req, res) => {
   req.app.db.collection('posts')
   .deleteOne({
-    "_id" : ObjectId(req.body.id),
-    "author_id" : ObjectId(req.body.my_id)
+    '_id': ObjectId(req.body.id)
   }, (err, result) => {
-    if (err) throw err
-    res.json(result)
+    if (err) res.json({'status': 'fail', 'data': err})
+    else res.json({'status': 'success', 'data': result})
   })
 }
 
 postController.update = (req, res) => {
   req.app.db.collection('posts')
-  .updateOne({ "_id" : ObjectId(req.body.id) }, {$set : req.body.values}, (err, result) => {
-    if (err) throw err
-    console.log('Updated one document')
-    res.json(result)
+  .updateOne({ '_id': ObjectId(req.body.id) }, {$set: req.body.values}, (err, result) => {
+    if (err) res.json({'status': 'fail', 'data': err})
+    else res.json({'status': 'success', 'data': result})
   })
 }
 
 postController.getMyPosts = (req, res) => {
   req.app.db.collection('posts')
-  .find({"author_id" : ObjectId(req.query.author_id)})
+  .find({'author_id': ObjectId(req.query.id)})
   .toArray((err, result) => {
-    if (err) throw err
-    console.log('Retrieved all posts from collection')
-    res.json(result)
+    if (err) res.json({'status': 'fail', 'data': err})
+    else res.json({'status': 'success', 'data': result})
   })
 }
 
 postController.updateScore = (req, res) => {
   req.app.db.collection('posts')
-  .updateOne({"_id" : ObjectId(req.body.id)}, {$set : {"score" : req.body.score}}, (err, result) => {
+  .updateOne({'_id': ObjectId(req.body.id)}, {$set: {'score': req.body.score}}, (err, result) => {
     if (err) throw err
     res.json(result)
   })
