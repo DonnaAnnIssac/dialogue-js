5f83719f92b1a1eb48ea15ff75a328647d1bf48d
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-26 11:11:23 +0530

    Define uniform response body format and pass callback to validateName function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,40 +1,41 @@
 const UserModel = require('./../models').user
 const userController = {}
 
-userController.post = (req, res) => {
-  if(validateName(req)) {
-    const user = new UserModel(req.body)
-    req.app.db.collection('users').insertOne(user, (err, result) => {
-      if (err) throw err
-      console.log('Document inserted')
-      res.json(result)
-    })
-  }
-  else res.json("This user name exists. Enter another name")
+userController.create = (req, res) => {
+  validateName(req, (err, available) => {
+    if (err) throw err
+    if (available) {
+      const user = new UserModel(req.body)
+      req.app.db.collection('users').insertOne(user, (err, result) => {
+        if (err) throw err
+        res.json({'status': 'success', 'data': 'Done. Sign In to continue'})
+      })
+    } else res.json({'status': 'fail', 'data': 'This user name exists. Enter another name'})
+  })
 }
 
-const validateName = (req) => {
+const validateName = (req, cb) => {
   req.app.db.collection('users')
-  .find({"userName" : req.body.userName})
+  .find({'userName': req.body.userName})
   .toArray((err, result) => {
     if (err) throw err
-    if(result.length === 0) return false
-    else return true
+    if (result.length === 0) cb(err, true)
+    else cb(err, false)
   })
 }
 
 userController.login = (req, res) => {
   req.app.db.collection('users')
-  .find({"userName" : req.body.userName})
+  .find({'userName': req.body.userName})
   .toArray((err, result) => {
     if (err) throw err
-    if (result.length === 0) res.json({"status" : 00, "value": "Invalid User Name"})
-    else if (result[0].password !== req.body.password) res.json({"status" : 10, "value" : "Password incorrect"})
+    if (result.length === 0) res.json({'status': 'fail', 'data': 'Invalid User Name'})
+    else if (result[0].password !== req.body.password) res.json({'status': 'fail', 'data': 'Password incorrect'})
     else {
       let userInfo = {}
       userInfo['userName'] = result[0].userName
       userInfo['_id'] = result[0]._id
-      res.json({"status" : 11, "value" : userInfo})
+      res.json({'status': 'success', 'data': userInfo})
     }
   })
 }
