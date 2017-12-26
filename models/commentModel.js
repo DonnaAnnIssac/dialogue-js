37586e7450b54cab866395232af1256d8deef5f4
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-26 12:21:23 +0530

    Add standardJS styling

--- models/commentModel.js
+++ models/commentModel.js
@@ -1,14 +1,14 @@
 const ObjectId = require('mongodb').ObjectID
 
 class Comment {
-    constructor(obj) {
-        this.link = obj.link,
-        this.text = obj.text,
-        this.author = obj.author,
-        this.author_id = ObjectId(obj.user_id)
-        this.post_id = ObjectId(obj.post_id),
-        this.score = 0
-    }
- }
+  constructor (obj) {
+    this.link = obj.link
+    this.text = obj.text
+    this.author = obj.author
+    this.author_id = ObjectId(obj.user_id)
+    this.post_id = ObjectId(obj.post_id)
+    this.score = 0
+  }
+}
 
- module.exports = Comment
+module.exports = Comment
