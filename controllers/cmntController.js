ff061730a1a65f71350adcdf4162dc5617a202b1
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-26 12:55:34 +0530

    Send success and fail status responses

--- controllers/cmntController.js
+++ controllers/cmntController.js
@@ -1,38 +1,36 @@
-
+const ObjectId = require('mongodb').ObjectID
 const CommentModel = require('./../models').comment
 const commentController = {}
 
-commentController.post = (req, res) => {
-  const comment  = new CommentModel(req.body)
+commentController.create = (req, res) => {
+  const comment = new CommentModel(req.body)
   req.app.db.collection('comments').insertOne(comment, (err, result) => {
-    if (err) throw err
-    console.log('Document inserted')
-    res.json(result)
+    if (err) res.json({'status': 'fail', 'data': err})
+    else res.json({'status': 'success', 'data': result})
   })
 }
 
 commentController.delete = (req, res) => {
   req.app.db.collection('comments')
   .deleteOne({
-    "_id" : ObjectId(req.body.id),
-    "author_id" : ObjectId(req.body.my_id)
+    '_id': ObjectId(req.body.id)
   }, (err, result) => {
-    if (err) throw err
-    res.json(result)
+    if (err) res.json({'status': 'fail', 'data': err})
+    else res.json({'status': 'success', 'data': result})
   })
 }
 
 commentController.update = (req, res) => {
   req.app.db.collection('comments')
-  .updateOne({ "_id" : ObjectId(req.body.id) }, {$set : req.body.values}, (err, result) => {
-    if (err) throw err
-    console.log('Updated one document')
+  .updateOne({ '_id': ObjectId(req.body.id) }, {$set: req.body.values}, (err, result) => {
+    if (err) res.json({'status': 'fail', 'data': err})
+    else res.json({'status': 'success', 'data': result})
   })
 }
 
 commentController.updateScore = (req, res) => {
   req.app.db.collection('comments')
-  .updateOne({"_id" : ObjectId(req.body.id)}, {$set : {"score" : req.body.score}}, (err, result) => {
+  .updateOne({'_id': ObjectId(req.body.id)}, {$set: {'score': req.body.score}}, (err, result) => {
     if (err) throw err
     res.json(result)
   })
