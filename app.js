67ac08c73305b5e0c7e0f8d2b711e97211513777
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-27 10:58:21 +0530

    Extract init data from config.js

--- app.js
+++ app.js
@@ -3,22 +3,32 @@ const app = express()
 const routes = require('./routes')
 const bodyParser = require('body-parser')
 const session = require('express-session')
-const cookieParser = require('cookie-parser')
-
+const MongoStore = require('connect-mongo')
+const config = require('./config.js')
 app.use(bodyParser.json())
-app.use(session({secret: 'secret'}))
+
+// function sessionManagementConfig (app) {
+//   app.use(session({
+//     store: new MongoStore({
+//       dbPromise: ''
+//     }),
+//     secret: 'secret',
+//     resave: false,
+//     saveUninitialized: true
+//   }))
+// }
+
 let MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb://localhost:27017/redditClone', (err, database) => {
+MongoClient.connect('mongodb://' + config.db.host + ':' + config.db.port + '/' + config.db.name, (err, database) => {
   if (err) console.error(err)
-
-  app.db = database.db('redditClone')
+  app.db = database.db(config.db.name)
   console.log('Connected to database')
 
   app.use('/api', routes)
   app.use(express.static('static'))
 
-  let server = app.listen(5000, () => {
+  let server = app.listen(config.app.port, () => {
     let port = server.address().port
     console.log('Express server listening on port %s', port)
   })
