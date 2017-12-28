a7569fec8480ccd63bc1b999a98d7f27a0430c4a
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-28 15:24:07 +0530

    Check if session exists for every method on controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,78 +3,92 @@ const PostModel = require('./../models').post
 const postController = {}
 
 postController.create = (req, res) => {
-  const post = new PostModel(req.body)
-  req.app.db.collection('posts').insertOne(post, (err, result) => {
-    if (err) res.json({'status': 'fail', 'data': err})
-    else res.json({'status': 'success', 'data': result})
-  })
+  if (req.session && req.session.userName === req.query.userName && req.session.userID === req.query.id) {
+    const post = new PostModel(req.body)
+    req.app.db.collection('posts').insertOne(post, (err, result) => {
+      if (err) throw err
+      else res.json({'status': 'success', 'data': result})
+    })
+  } else res.json({'status': 'fail', 'data': 'You need to be logged in first'})
 }
 
 postController.getAll = (req, res) => {
   if (req.session && req.session.userName === req.query.userName && req.session.userID === req.query.id) {
     req.app.db.collection('posts').find().toArray((err, result) => {
-      if (err) res.json({'status': 'fail', 'data': err})
+      if (err) throw err
       else res.json({'status': 'success', 'data': result})
     })
   } else res.json({'status': 'fail', 'data': 'You need to be logged in first'})
 }
 
 postController.show = (req, res) => {
-  req.app.db.collection('posts')
-  .find({'_id': ObjectId(req.query.id)})
-  .toArray((err, result) => {
-    if (err) res.json({'status': 'fail', 'data': err})
-    else {
-      req.app.db.collection('comments')
-      .find({'post_id': ObjectId(req.query.id)})
-      .toArray((err, results) => {
-        if (err) res.json({'status': 'fail', 'data': {'post': result[0], 'comments': err}})
-        else res.json({'status': 'success', 'data': {'post': result[0], 'comments': results}})
-      })
-    }
-  })
+  if (req.session && req.session.userName === req.query.userName && req.session.userID === req.query.userID) {
+    req.app.db.collection('posts')
+    .find({'_id': ObjectId(req.query.id)})
+    .toArray((err, result) => {
+      if (err) throw err
+      else {
+        req.app.db.collection('comments')
+        .find({'post_id': ObjectId(req.query.id)})
+        .toArray((err, results) => {
+          if (err) throw err
+          else res.json({'status': 'success', 'data': {'post': result[0], 'comments': results}})
+        })
+      }
+    })
+  } else res.json({'status': 'fail', 'data': 'You need to be logged in first'})
 }
 
 postController.delete = (req, res) => {
-  req.app.db.collection('posts')
-  .deleteOne({
-    '_id': ObjectId(req.body.id)
-  }, (err, result) => {
-    if (err) res.json({'status': 'fail', 'data': err})
-    else res.json({'status': 'success', 'data': result})
-  })
+  if (req.session && req.session.userName === req.query.userName && req.session.userID === req.query.id) {
+    req.app.db.collection('posts')
+    .deleteOne({
+      '_id': ObjectId(req.body.id)
+    }, (err, result) => {
+      if (err) throw err
+      else res.json({'status': 'success', 'data': result})
+    })
+  } else res.json({'status': 'fail', 'data': 'You need to be logged in first'})
 }
 
 postController.update = (req, res) => {
-  req.app.db.collection('posts')
-  .updateOne({ '_id': ObjectId(req.body.id) }, {$set: req.body.values}, (err, result) => {
-    if (err) res.json({'status': 'fail', 'data': err})
-    else res.json({'status': 'success', 'data': result})
-  })
+  if (req.session && req.session.userName === req.query.userName && req.session.userID === req.query.id) {
+    req.app.db.collection('posts')
+    .updateOne({ '_id': ObjectId(req.body.id) }, {$set: req.body.values}, (err, result) => {
+      if (err) throw err
+      else res.json({'status': 'success', 'data': result})
+    })
+  } else res.json({'status': 'fail', 'data': 'You need to be logged in first'})
 }
 
 postController.getMyPosts = (req, res) => {
-  req.app.db.collection('posts')
-  .find({'author_id': ObjectId(req.query.id)})
-  .toArray((err, result) => {
-    if (err) res.json({'status': 'fail', 'data': err})
-    else res.json({'status': 'success', 'data': result})
-  })
+  if (req.session && req.session.userName === req.query.userName && req.session.userID === req.query.id) {
+    req.app.db.collection('posts')
+    .find({'author_id': ObjectId(req.query.id)})
+    .toArray((err, result) => {
+      if (err) res.json({'status': 'fail', 'data': err})
+      else res.json({'status': 'success', 'data': result})
+    })
+  } else res.json({'status': 'fail', 'data': 'You need to be logged in first'})
 }
 
 postController.upvote = (req, res) => {
-  req.app.db.collection('posts')
-  .updateOne({'_id': ObjectId(req.body.id)}, {$inc: {'upVote': 1}}, (err, result) => {
-    if (err) res.json({'status': 'fail', 'data': err})
-    else res.json({'status': 'success', 'data': result})
-  })
+  if (req.session && req.session.userName === req.query.userName && req.session.userID === req.query.id) {
+    req.app.db.collection('posts')
+    .updateOne({'_id': ObjectId(req.body.id)}, {$inc: {'upVote': 1}}, (err, result) => {
+      if (err) res.json({'status': 'fail', 'data': err})
+      else res.json({'status': 'success', 'data': result})
+    })
+  } else res.json({'status': 'fail', 'data': 'You need to be logged in first'})
 }
 
 postController.downvote = (req, res) => {
-  req.app.db.collection('posts')
-  .updateOne({'_id': ObjectId(req.body.id)}, {$inc: {'downVote': 1}}, (err, result) => {
-    if (err) res.json({'status': 'fail', 'data': err})
-    else res.json({'status': 'success', 'data': result})
-  })
+  if (req.session && req.session.userName === req.query.userName && req.session.userID === req.query.id) {
+    req.app.db.collection('posts')
+    .updateOne({'_id': ObjectId(req.body.id)}, {$inc: {'downVote': 1}}, (err, result) => {
+      if (err) res.json({'status': 'fail', 'data': err})
+      else res.json({'status': 'success', 'data': result})
+    })
+  } else res.json({'status': 'fail', 'data': 'You need to be logged in first'})
 }
 module.exports = postController
