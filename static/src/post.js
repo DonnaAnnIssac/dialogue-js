fcbf72573510084392c6ab59d3f5ed48abfabdfd
Donna Ann Issac <donna.ann1209@gmail.com>
2017-12-28 15:26:32 +0530

    Add user name and id as query string params and refactor code

--- static/src/post.js
+++ static/src/post.js
@@ -1,61 +1,44 @@
-const createTextDiv = (element, fieldName) => {
+const createTextDiv = (element, fieldName, container) => {
   let field = document.createElement('div')
   field.textContent = element[fieldName]
-  return field
+  container.appendChild(field)
 }
 
-const createLinkField = (element) => {
+const createLinkField = (element, container) => {
   let link = document.createElement('div')
   let linkVal = document.createElement('a')
   linkVal.setAttribute('href', element.link)
   linkVal.setAttribute('target', '_blank')
   linkVal.innerHTML = 'click here'
   link.appendChild(linkVal)
-  return link
+  container.appendChild(link)
 }
 
 const createContainerDiv = (container, element) => {
-  let author = createTextDiv(element, 'author')
-  container.appendChild(author)
-  let title = createTextDiv(element, 'title')
-  container.appendChild(title)
-  if (element.text !== null) {
-    let text = createTextDiv(element, 'text')
-    container.appendChild(text)
-  }
-  if (element.link !== null) {
-    let link = createLinkField(element)
-    container.appendChild(link)
-  }
+  createTextDiv(element, 'author', container)
+  createTextDiv(element, 'title', container)
+  if (element.text !== null) createTextDiv(element, 'text', container)
+  if (element.link !== null) createLinkField(element, container)
   return container
 }
 
-const showPost = (postObj) => {
-  console.log(postObj)
-  let postContainer = document.getElementById('postContainer')
-  postContainer = createContainerDiv(postContainer, postObj)
-}
+const showPost = postObj => createContainerDiv(document.getElementById('postContainer'), postObj)
 
-const showComments = (commentsList) => {
-  let commentsContainer = document.getElementById('commentContainer')
-  commentsList.forEach((comment) => {
-    commentsContainer = createContainerDiv(commentsList)
-  })
-}
+const showComments = commentsList => commentsList.forEach(comment => createContainerDiv(document.getElementById('commentContainer'), commentsList))
 
 const getPost = () => {
   let xhr = new XMLHttpRequest()
   let url = window.location.href
   let queryString = url.slice(url.indexOf('?') + 1)
-  console.log(queryString)
-  xhr.open('GET', 'http://localhost:5000/api/post/show?' + queryString, true)
+  xhr.open('GET', 'http://localhost:5000/api/post/show?userName=' + sessionStorage.name + '&userID=' + sessionStorage.id + '&' + queryString, true)
   xhr.send()
   xhr.onreadystatechange = () => {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      let postDetails = JSON.parse(xhr.responseText)
-      console.log(postDetails)
-      showPost(postDetails.post)
-      showComments(postDetails.comments)
+      if (JSON.parse(xhr.responseText).status === 'success') {
+        let postDetails = JSON.parse(xhr.responseText).data
+        showPost(postDetails.post)
+        showComments(postDetails.comments)
+      } else window.location.href = 'index.html'
     }
   }
 }
